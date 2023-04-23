307cc6ed2e0ff81a3b6d861fb797625c1c15a3cb
Peter Key <petekey@duck.com>
2023-04-24 07:06:57 +1000

    Videos added to playlist display as cards in playlist column

--- src/Components/Playlist.js
+++ src/Components/Playlist.js
@@ -1,10 +1,30 @@
+import { Track } from './Track'
+
 export function Playlist({playlistVideos}) {
+
+    // const thePlaylist = {playlistVideos}
+
+    // useEffect(() => {
+
+    // })
+
+    const cards = []
+
+    playlistVideos.map(video => {
+        cards.push(
+            <Track 
+                video={video}
+            />
+        )
+    })
+
     return (
         <div className="column-container">
             <h2>
                 <input type="text" placeholder="New playlist" className="Playlist-input"></input>
             </h2>
-            {playlistVideos ? 'Add a video to playlist...' : playlistVideos}
+            {console.log(playlistVideos.length > 0)}
+            {playlistVideos.length === 0 ? 'Add a video to playlist...' : cards}
         </div>
     )
 }
